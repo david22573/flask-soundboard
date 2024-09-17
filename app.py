a50a26d09daa3b6d50f63373ed2d8a8f3d347f0f
David Rivero <davidmiguel22573@gmail.com>
2024-09-16 21:49:47 -0700

    ai layout

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.get("/")
-def hello():
-    return render_template("index.html")
+@app.route("/")
+def index():
+    audio_files = [f for f in os.listdir("static/audio") if f.endswith(".mp3")]
+    return render_template("index.html", audio_files=audio_files)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
